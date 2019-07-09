6f0be4df3fdde46fbfa653746b8ced9325152532
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-07-09 14:07:48 +0100

    create page title component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import FormContainer from './Components/Form';
+import PageTitle from './Components/PageTitle';
 // import './App.css';
 
 const App = () => {
-  return <FormContainer />;
+  return (
+    <>
+      <PageTitle />
+      <FormContainer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/PageTitle/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const PageTitle = () => <Title>Github Profile Card App</Title>;
+
+export default PageTitle;
